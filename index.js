a0631632430d24fc9962de9c8d741a719be71888
Kashif <kashifnezam123@gmail.com>
2022-08-09 20:48:13 +0530

    let, const, var difference

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-//"use strict"; we can use it to declare variable strictly
+/*"use strict";
+we can use it to declare variable strictly
 console.log("Hello World");
 k = "ymh";
-console.log(k);
+console.log(k);*/
+// differce b/w var let const
+
+var name = "kuch";
+console.log(name);
+var name = "nhy"; // here we can redefine the variable
+console.log(name);
+
+let age = 20;
+//let age = 40; it will give error we can't redefine the variable
+age = 50; // but can reassign 
+console.log(age);
+const add = "India" //const must initialise, cant redefine and can't reassign 
+console.log(add);
