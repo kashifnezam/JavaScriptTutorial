224df2d947255f77cf3e0da74d0ee9deba5f930e
Kashif <kashifnezam123@gmail.com>
2022-08-12 21:18:49 +0530

    arrow function and its shortcut

---
+++ index5.js
@@ -0,0 +1,27 @@
+// function shortcut and Arrow function
+
+/*const isEven = function(num){
+  if(num%2 == 0){
+    return true;
+  }
+  return false;
+};
+
+console.log(isEven(56));*/
+
+//ARROW FUNCTION
+/*
+const isEven = (num) => {
+  return num%2 == 0;
+};
+console.log(isEven(44));
+console.log(isEven(43));
+
+// more shortcut arrow function
+
+const isOdd = num => num%2 != 0;
+console.log(isOdd(47));*/
+
+const firstString = (a) => a[0];
+
+console.log(firstString("Kashif"));
